76032be6cc133fe3e90d758c3063b5763d66c854
Amaya <amayaabaker@gmail.com>
2026-02-02 17:24:25 -0500

    did task 1... can't check, error msg: "Could not load the Qt platform plugin "xcb" even though it was found. This application failed to start because no Qt platform plugin could be initialized. Aborted (core dump)"

--- src/task 1/task1.py
+++ src/task 1/task1.py
@@ -5,3 +5,44 @@
 # 1. A constructor
 # 2. One class method to display the frames
 ###########################
+import numpy as np
+import cv2 as cv
+import os 
+
+class VideoPlayer:
+    def __init__(self):
+        cdir=os.getcwd()
+        self.cap=cv.VideoCapture(cdir+"/src/test_video.mp4") # reads video file
+
+    def dispFrames(self):
+        if not self.cap.isOpened(): # if vid is not ok
+            print("can't open video.") 
+            exit() # if it is
+
+
+        # read & display frames
+        while True: 
+            #returns boolean & frame
+            ret,frame = self.cap.read()
+
+            if not ret: # if vid don't start or ends
+                print("can't recieve frame.")
+                break
+            
+            # displays frame
+            cv.imshow('frame', frame)
+
+            # to leave early press q
+            if cv.waitkey(1)==ord('q'):
+                break
+
+        self.cap.release()
+        cv.destroyAllWindows()
+
+    
+def main():
+    myVideo= VideoPlayer()
+    myVideo.dispFrames()
+
+if __name__=="__main__":
+    main()
